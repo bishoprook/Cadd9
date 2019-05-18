d0b5419315892aaba681e52c314c4da04a172864
Kyle Woodlock <woodlock@squareup.com>
2019-05-18 16:57:43 -0400

    Test updates for IntExtensionsTest

--- test/Util/IntExtensionsTest.cs
+++ test/Util/IntExtensionsTest.cs
@@ -3,15 +3,25 @@ using Xunit;
 
 public class IntExtensionsTest
 {
-    [Fact]
-    public void NegModWorks()
+    [Theory]
+    [InlineData(11, 3, 2)]
+    [InlineData(-8, 3, 1)]
+    [InlineData(0, 10, 0)]
+    [InlineData(5, 10, 5)]
+    [InlineData(10, 10, 0)]
+    public void TestModulus(int operand, int modulus, int expected)
     {
-        Assert.Equal(1, (-8).Modulus(3));
+        Assert.Equal(expected, operand.Modulus(modulus));
     }
 
-    [Fact]
-    public void ModWorks()
+    [Theory]
+    [InlineData(2, 10, 2)]
+    [InlineData(6, 10, -4)]
+    [InlineData(25, 6, 1)]
+    [InlineData(29, 6, -1)]
+    [InlineData(-19, 6, -1)]
+    public void TestDemodulus(int operand, int modulus, int expected)
     {
-        Assert.Equal(2, 11.Modulus(3));
+        Assert.Equal(expected, operand.Demodulus(modulus));
     }
 }
